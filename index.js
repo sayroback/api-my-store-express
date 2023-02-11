4c768afe689dbc25dc68cee19d5b11c880b93846
Juan Manuel Calderon Robles <sayroback@gmail.com>
2023-02-11 16:44:33 -0600

    productos fake

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { faker } = require("@faker-js/faker");
 const app = express();
 const port = 3005;
 
@@ -7,16 +8,17 @@ app.get("/", (request, response) => {
 });
 
 app.get("/products", (request, response) => {
-  response.json([
-    {
-      name: "Producto 1",
-      price: 1000,
-    },
-    {
-      name: "Producto 2",
-      price: 2000,
-    },
-  ]);
+  const products = [];
+  const { size } = request.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      image: faker.image.imageUrl(),
+    });
+  }
+  response.json(products);
 });
 
 app.get("/products/:id", (request, response) => {
@@ -28,6 +30,30 @@ app.get("/products/:id", (request, response) => {
       price: 1000,
     },
   ]);
+});
+
+// http://localhost:3005/categories/4123/products/12455
+app.get("/categories/:categoryId/products/:productId", (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
+  });
+});
+
+// http://localhost:3005/users?limit=122&offset=233
+app.get("/users", (request, response) => {
+  const { limit, offset } = request.query;
+  if (limit && offset) {
+    response.json([
+      {
+        limit,
+        offset,
+      },
+    ]);
+  } else {
+    response.send("No hay parámetros");
+  }
 });
 
 app.listen(port, () => {
